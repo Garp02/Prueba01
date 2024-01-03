4210b221238446b77ae3a7af88d051616e88e497
Garp02 <mr.ibra@ciencias.unam.mx>
2024-01-02 18:48:51 -0600

    feat: update P0.py

--- P0.py
+++ P0.py
@@ -1,3 +1,3 @@
 print("Hola mundo")
 print("La prueba ha empezado")
-print("Para todo épsilon mayor a cero existe un delta mayor a cero tal que si el valor absoluto de la diferencia de x y c es menor a delta, entonces la diferencia del valor absoluto de las imágenes de función evaluada en x y en c es menor que épsilon")
+print("Para todo épsilon mayor a cero existe un delta mayor a cero tal que si el valor absoluto de la diferencia de x y c es menor a delta, entonces la diferencia del valor absoluto de las imágenes de la función evaluada en x y en c es menor que épsilon")
